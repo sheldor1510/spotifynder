0e22faaf8485d02c2fbd978473179e9d46f85427
Anshul Saha <anshulsaha15@gmail.com>
2024-12-08 11:31:12 -0500

    <added accept request functionality><#15,#23>:<added a function to accept a request and update the object in the matches table with status accepted><to implement functionality of accepting a request>

--- backend/controllers/matchesController.js
+++ backend/controllers/matchesController.js
@@ -47,3 +47,50 @@ exports.fetchRequests = async (req, res) => {
         return res.status(500).send({ success: false, error });
     }
 }
+
+exports.acceptRequest = async (req, res) => {
+    // check if access token is provided
+    if (!req.query.accessToken) {
+        return res.status(400).send("Access Token missing");
+    }
+
+    // find user by access token
+    const userToken = req.query.accessToken;
+
+    // find user by access token
+    const user = await User.findOne({where: { accessToken: userToken} });
+
+    // if user does not exist, return an error
+    if(!user) {
+        return res.status(400).send("User does not exist");
+    }
+
+    // check if requestor id is provided
+    if (!req.query.requestorId) {
+        return res.status(400).send("Requestor ID missing");
+    }
+
+    try {
+        // find match where user.id is the user_requested and requestor is requestorId and status is pending
+        const match = await Match.findOne({
+            where: {
+                user_requested: user.id,
+                requestor: req.query.requestorId,
+                status: 'pending'
+            }
+        });
+
+        // if match does not exist, return an error
+        if (!match) {
+            return res.status(400).send("Match does not exist");
+        }
+
+        // update match status to accepted
+        match.status = 'accepted';
+        await match.save();
+
+        return res.status(200).send({ success: true, message: "Request accepted" });
+    } catch (error) {
+        return res.status(500).send({ success: false, error });
+    }
+}
